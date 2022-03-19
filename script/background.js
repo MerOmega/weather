8cf3464358cf305d8f47a4ac2aeeb6ab691bdee5
MerOmega <32043630+MerOmega@users.noreply.github.com>
2022-03-19 14:07:27 -0300

    Finished

--- script/background.js
+++ script/background.js
@@ -1,4 +1,11 @@
 const now = new Date();
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img")
+// ---------------- Background
 const body = document.querySelector("body");
 const twinky= document.querySelector(".twinkling");
 const stars= document.querySelector(".stars")
@@ -25,25 +32,76 @@ function setBack(){
         stars.classList.add("stars")
     }
 }
+setBack();
+//-------------------------------------------------------------------------------------------
 
+
+
+const updateCity = async(city)=>{
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    //shorthand
+    return {
+        cityDets,
+        weather
+    }
+}
+
+const updateUI= (data)=>{
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+    
+    //destructure
+    const{ cityDets,weather }=data;
+
+    //update templates
+    details.innerHTML=`
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>`;
+    //update icons
+    const iconSrc=`img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute("src",iconSrc)
+    //no se porque esta al reves
+    let timeSrc=null;
+    weather.IsDayTime? timeSrc = "img/day.svg":timeSrc="img/night.svg";
+    time.setAttribute("src",timeSrc);
+
+}
 const mapa = ()=>{
-        navigator.geolocation.getCurrentPosition(data=>{
-            let place = new Place(data.coords.latitude,data.coords.longitude);
-            console.log(place)
-            localStorage.setItem("MyPlace",JSON.stringify(place));
-        })
+    navigator.geolocation.getCurrentPosition(data=>{
+        let place = new Place(data.coords.latitude,data.coords.longitude);
+        console.log(place)
+        localStorage.setItem("MyPlace",JSON.stringify(place));
+    })
 };
 
 const say = async ()=>{ 
-    // const response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=-34.9321,-57.9533&key=AIzaSyA1ne0B5mpGle2-P_YgGuYZ5tFRbJHt8NA')
-    let newObj = JSON.parse(localStorage.getItem("MyPlace"));
-    const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${newObj.lat}&lon=${newObj.lng}&format=json&zoom=10`)
-    const data = await response.json();   
-    return data 
+// const response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=-34.9321,-57.9533&key=AIzaSyA1ne0B5mpGle2-P_YgGuYZ5tFRbJHt8NA')
+let newObj = JSON.parse(localStorage.getItem("MyPlace"));
+const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${newObj.lat}&lon=${newObj.lng}&format=json&zoom=10`)
+const data = await response.json();   
+return data 
 }
 
+
+
 //data.plus_code.compound_code
-say().then(data=>{
-    console.log(data.address.city)
+say()
+    .then(data=>updateCity(data.address.city)) //.then(data=>{updateCity(data.address.city)}) NO USAR {} intenta resolver la siguiente promesa sin valor
+    .then(data=>updateUI(data))
+    .catch(err=>console.log(err))
+
+
+cityForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+    // update ui
+    updateCity(city)
+    .then(data=>updateUI(data))
+    .catch(err=>console.log(err));
 })
-setBack();
--- script/forecast.js
+++ script/forecast.js
@@ -1,1 +1,22 @@
 const key = "zHR8aBbYlpsEtCczgzQ8EkTaw2LztGpc";
+
+
+
+const getCity = async(city)=>
+{
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+const getWeather = async(id)=>{
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`;
+    const query = `?apikey=${key}`;
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return data[0];
+}
